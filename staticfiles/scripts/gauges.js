287d009511314e81932a47e2e6009b4757c8239b
daniel payne <haroldfranklinpayne@gmail.com>
2020-08-30 10:57:28 -0400

    adjusted stylings added comments

--- staticfiles/scripts/gauges.js
+++ staticfiles/scripts/gauges.js
@@ -1,6 +1,5 @@
-
 /*Import google charts for gauges*/
-google.charts.load('current', {'packages':['gauge']});
+google.charts.load("current", { packages: ["gauge"] });
 google.charts.setOnLoadCallback(drawChart);
 
 // grab dom elements for text-based stats
@@ -12,52 +11,63 @@ diskAvail = document.querySelector("#diskAvailable");
 diskUsed = document.querySelector("#diskUsed");
 
 function drawChart() {
-
     var data = google.visualization.arrayToDataTable([
-        ['Label', 'Value'],
-        ['Memory', 0],
-        ['CPU', 0],
-        ['Disk', 0]
+        ["Label", "Value"],
+        ["Memory", 0],
+        ["CPU", 0],
+        ["Disk", 0]
     ]);
 
     var options = {
-        width: 500, height: 220,
-        redFrom: 90, redTo: 100,
-        yellowFrom:75, yellowTo: 90,
+        width: 500,
+        height: 220,
+        redFrom: 90,
+        redTo: 100,
+        yellowFrom: 75,
+        yellowTo: 90,
         minorTicks: 5
     };
 
-    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+    var chart = new google.visualization.Gauge(
+        document.getElementById("chart_div")
+    );
 
     chart.draw(data, options);
 
     // API call for data updates
-    (async function pullStats(){
-        setTimeout(async()=>{
-            await $.ajax({url:'https://cpu-stats-api-heroku.herokuapp.com/api/?format=json',
-                success:function(api_data){
-                    //update dashboard
+    (async function pullStats() {
+        setTimeout(async () => {
+            await axios
+                .get("https://cpu-stats-api-heroku.herokuapp.com/api/?format=json")
+                .then(api_data => {
+                    // Update dashboard
                     // Set Available Memory Gauge
-                    data.setValue(0,1,api_data.usedMemoryPercent);
+                    data.setValue(0, 1, api_data.data.usedMemoryPercent);
                     chart.draw(data, options);
                     // Set available CPU Gauge
-                    data.setValue(1, 1, api_data.CPUUsedPercent);
+                    data.setValue(1, 1, api_data.data.CPUUsedPercent);
                     chart.draw(data, options);
                     // Set disk space used gauge
-                    data.setValue(2, 1, api_data.usedDiskSpacePercent);
+                    data.setValue(2, 1, api_data.data.usedDiskSpacePercent);
                     chart.draw(data, options);
                     // Push data to text-based stat elements
-                    memAvail.textContent = "Memory Available: " + api_data.availableMemoryPercent + "%";
-                    memUsed.textContent = "Memory Used: " + api_data.usedMemoryPercent + "%";
-                    cpuAvail.textContent = "CPU Available: " + api_data.CPUAvailablePercent + "%";
-                    cpuUsed.textContent = "CPU Used: " + api_data.CPUUsedPercent + "%"
-                    diskAvail.textContent = "Disk Space Available: " + api_data.availableDiskSpacePercent + "%";
-                    diskUsed.textContent = "Disk Space Used: " + api_data.usedDiskSpacePercent + "%";
+                    memAvail.textContent =
+                        "Memory Available: " + api_data.data.availableMemoryPercent + "%";
+                    memUsed.textContent =
+                        "Memory Used: " + api_data.data.usedMemoryPercent + "%";
+                    cpuAvail.textContent =
+                        "CPU Available: " + api_data.data.CPUAvailablePercent + "%";
+                    cpuUsed.textContent = "CPU Used: " + api_data.data.CPUUsedPercent + "%";
+                    diskAvail.textContent =
+                        "Disk Space Available: " + api_data.data.availableDiskSpacePercent + "%";
+                    diskUsed.textContent =
+                        "Disk Space Used: " + api_data.data.usedDiskSpacePercent + "%";
                     //setup next poll recursively
                     pullStats();
-                },
-                dataType:"json"});
-        },100);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }, 100);
     })();
-
 }
--- stats_api/cpuStats.py
+++ stats_api/cpuStats.py
@@ -1,24 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import functools
-import psutil,os,datetime
+import psutil
+import os
+import datetime
 
 
 def proper_round(num, dec=1):
-    num = str(num)[:str(num).index('.')+dec+2]
-    if num[-1]>='5':
-        return float(num[:-2-(not dec)]+str(int(num[-2-(not dec)])+1))
+    num = str(num)[:str(num).index('.') + dec + 2]
+    if num[-1] >= '5':
+        return float(num[:-2 - (not dec)] + str(int(num[-2
+                     - (not dec)]) + 1))
     return float(num[:-1])
 
 
-class GetStats():
+class GetStats:
+
     """Class to get stats for host system"""
 
     def getRam(self):
         usedRam = psutil.virtual_memory().percent
-        availableRam = (psutil.virtual_memory().available * 100) / psutil.virtual_memory().total
-        return [{
-        'usedRam':usedRam,
-        'availableRam':proper_round(availableRam)
-        }]
+        availableRam = psutil.virtual_memory().available * 100 \
+            / psutil.virtual_memory().total
+        return [{'usedRam': usedRam,
+                'availableRam': proper_round(availableRam)}]
 
     def getDiskInfo(self):
         return psutil.disk_usage('/')
@@ -26,11 +31,15 @@ class GetStats():
     def getCPU(self):
         psutil.cpu_percent(interval=None)
         cpuUsed = psutil.cpu_percent(interval=1, percpu=True)
-        result = functools.reduce(lambda a,b : a+b,cpuUsed)
-        return float('{:0.2f}'.format(result/psutil.cpu_count(logical=True)))
+        result = functools.reduce(lambda a, b: a + b, cpuUsed)
+        return float('{:0.2f}'.format(result
+                     / psutil.cpu_count(logical=True)))
 
     def getTemps(self):
-        return 'Temperature sensors not supported on current host' if psutil.sensors_temperatures() == {} else psutil.sensors_temperatures()
+        return ('Temperature sensors not supported on current host'
+                 if psutil.sensors_temperatures()
+                == {} else psutil.sensors_temperatures())
 
     def getUpTime(self):
-        return datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S'
+                )
